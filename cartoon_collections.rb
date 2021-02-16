c1d3c57bc39589af59522c8f2931477571c3f84e
Henry Velasquez <hvelas93@gmail.com>
2021-02-16 14:45:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,7 +36,7 @@ def find_valid_calls(planeteer_calls)
   for i in valid_calls do
     x=planeteer_calls.find{|i|planeteer_calls.include? i}
     p x
-p x==true
+p x==false
 
       if x==true
           #binding.pry
